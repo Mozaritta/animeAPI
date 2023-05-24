dce21beb1e96c56fdab1d0b620fcb2beaafd0040
Mozaritta <mozaritta@gmail.com>
2023-05-24 17:24:19 +0000

    Add entities

---
+++ src/main/java/api/mozaritta/movies/entities/Anime.java
@@ -0,0 +1,34 @@
+package api.mozaritta.movies.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "anime")
+public class Anime {
+
+    @Id
+    private ObjectId id;
+    private String title;
+    private String type;
+    private String episodes;
+    private String status;
+    private String thumbnail;
+    private String picture;
+    private AnimeSeason animeSeason;
+    private List<String> sources;
+    private List<String> tags;
+    @DocumentReference
+    private List<Review> reviewsIds;
+    
+}
